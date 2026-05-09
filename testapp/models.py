7a7c70c8083dd7ac3f1b35992602394ca52fd9a2
Sumarokova <sumarokova75@internet.ru>
2026-05-09 17:47:49 +0300

    My 8nth homework on Django: creating Categoty, Task, Subtask

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
+class Category(models.Model):
+     name = models.CharField(max_length=50, unique=True)
+
+     def __str__(self):
+          return self.name
+
+
 # Create your models here.
+class Task(models.Model):
+     title = models.CharField(max_length=30)
+     description = models.TextField()
+     categories = models.ManyToManyField(Category, blank=True)
+
+     STATUS_CHOICES = (
+          ("New", "New"),
+          ("In progress", "In progress"),
+          ("Pending", "Pending"),
+          ("Blocked", "Blocked"),
+          ("Done", "Done"),
+     )
+     status = models.CharField(STATUS_CHOICES, default='New')
+     deadline = models.DateTimeField()
+     created_at = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+          return self.title
+
+
+class SubTask(models.Model):
+     title = models.CharField(max_length=30)
+     description = models.TextField()
+     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+     STATUS_CHOICES = (
+          ("New", "New"),
+          ("In progress", "In progress"),
+          ("Pending", "Pending"),
+          ("Blocked", "Blocked"),
+          ("Done", "Done"),
+     )
+     status = models.CharField(STATUS_CHOICES,
+                             default='New')
+     deadline = models.DateTimeField()
+     created_at = models.DateTimeField(auto_now_add=True)
+
+     def __str__(self):
+          return self.title
+
